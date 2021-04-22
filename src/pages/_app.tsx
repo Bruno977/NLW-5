59d26d8acdc8ecb1b22c17600e9f06ea91d2761c
Bruno <bruno_151997@hotmail.com>
2021-04-22 14:47:22 -0300

    update playercontext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,34 @@
 import '../styles/globals.scss'
 
 import { Header } from '../components/Header'
+import {PlayerContext} from '../contexts/PlayerContext'
+import styles from '../styles/app.module.scss'
+import { useState } from 'react'
 import { Player } from '../components/Player'
 
-import styles from '../styles/app.module.scss'
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className={styles.wrapper}>
-      <main>
-      <Header />
 
-      <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode){
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0);
+  }
+
+  return (
+
+    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, play}}>
+      <div className={styles.wrapper}>
+        <main>
+        <Header />
+
+        <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
+
     )
 }
 
